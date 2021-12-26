0fc40d8a56e77eba174df4c6102351eea88507ec
omnrohr <67183475+omnrohr@users.noreply.github.com>
2021-12-26 22:36:08 +0200

    filter results by status

--- todo_list/views.py
+++ todo_list/views.py
@@ -5,6 +5,8 @@ from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.views import LoginView as BaseLoginView
 
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .models import TodoList
 from django.views import View
 from django.urls import reverse_lazy
@@ -21,27 +23,42 @@ class LoginView(BaseLoginView):
         return reverse_lazy('tasks')
 
 
-class TasksViewList(ListView):
+class TasksViewList(LoginRequiredMixin, ListView):
     model = TodoList
     context_object_name = 'tasks'
-    paginate_by = 5
     ordering = ['created']
 
-class TaskDetailView(DetailView):
+    def get_queryset(self):
+        return TodoList.objects.filter(user=self.request.user)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['completed'] = TodoList.objects.filter(user=self.request.user, completed=True)
+        context['completed_count'] = context['completed'].count()
+        context['in_porgress'] = TodoList.objects.filter(user=self.request.user, completed=False)
+        context['in_porgress_count'] = context['in_porgress'].count()
+        return context
+
+class TaskDetailView(LoginRequiredMixin, DetailView):
     model = TodoList
     context_object_name = 'task_detail'
 
 
-class CreateTaskView(CreateView):
+class CreateTaskView(LoginRequiredMixin, CreateView):
     model = TodoList
     fields = ['user', 'title', 'description', 'completed']
     success_url = reverse_lazy('tasks')
 
-class UpdateTaskView(UpdateView):
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class UpdateTaskView(LoginRequiredMixin, UpdateView):
     model = TodoList
     fields = ['user', 'title', 'description', 'completed']
     success_url = reverse_lazy('tasks')
-class DeleteTaskView(DeleteView):
+class DeleteTaskView(LoginRequiredMixin, DeleteView):
     model = TodoList
     success_url = '/'
 
