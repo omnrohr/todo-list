03acf763471f9dcf315f9492ed8bc5b3d528e4cd
omnrohr <67183475+omnrohr@users.noreply.github.com>
2021-12-26 20:24:27 +0200

    login and logout view added

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,7 +1,11 @@
 from django.urls.conf import path
-from .views import DeleteTaskView, TasksViewList, TaskDetailView, CreateTaskView, UpdateTaskView
+from .views import DeleteTaskView, LoginView, TasksViewList, TaskDetailView, CreateTaskView, UpdateTaskView
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', TasksViewList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetailView.as_view(), name='task'),
     path('create/', CreateTaskView.as_view(), name='create-task'),
--- todo_list/views.py
+++ todo_list/views.py
@@ -3,12 +3,24 @@ from django.shortcuts import HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.views import LoginView as BaseLoginView
 
 from .models import TodoList
 from django.views import View
 from django.urls import reverse_lazy
 # Create your views here.
 from django.views.generic.edit import ModelFormMixin
+
+
+class LoginView(BaseLoginView):
+    template_name = 'todo_list/login.html'
+    redirect_authenticated_user = True
+    fields = '__all__'
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+
 class TasksViewList(ListView):
     model = TodoList
     context_object_name = 'tasks'
